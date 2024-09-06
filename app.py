1aacbc5791ea1e488202472f983931769e0d494c
JustAlip <arivinsantos1@gmail.com>
2024-09-06 08:50:29 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,7 @@
+def luas_segitiga():
+    a = int(input("Masukkan Bilangan Ke1 : "))
+    b = int(input("Masukkan Bilangan Ke1 : "))
+    luas = a * b /2
+    print("Luas Segitiga Adalah : ", luas)
+
+luas_segitiga()
